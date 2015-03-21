9a1e2f10babdee51589ed4ae657dc6ce5ad665e1
Warringer <Warringer@gmail.com>
2015-03-21 16:09:41 +0100

    Removed Controls.Hardware.Worker Added simple Controls.Touchscreen

---
+++ Controls/Touchscreen.py
@@ -0,0 +1,20 @@
+'''
+Created on Mar 21, 2015
+
+@author: warringer
+'''
+
+import pygame, time
+
+class Touchscreen(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, buttonlayout):
+        '''
+        Constructor
+        '''
+        self.buttonlayout = buttonlayout
+        
